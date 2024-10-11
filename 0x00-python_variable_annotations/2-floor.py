6dfb4255e7f1c3a71cd9b664017de9910ec60347
Tochukwu92 <Ilonubatochukwu@gmail.com>
2024-10-11 23:39:27 +0100

    python anotation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''takes float object and returns integer'''
+
+
+def floor(n: float) -> int:
+    '''
+    takes in a float object and return float.
+    '''
+    return int(n)
