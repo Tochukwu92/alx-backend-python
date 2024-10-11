d4fd981371281db67246589a37b1fa8ecfef2c2d
Tochukwu92 <Ilonubatochukwu@gmail.com>
2024-10-11 23:26:37 +0100

    python anotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''concatnate two strings and return concateneted string'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    concate - concatenate two strings passed as arguments
+    Return: concatened string
+    '''
+    return str1 + str2
