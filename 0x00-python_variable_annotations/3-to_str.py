723a4bf3d318a15722ecbf0ef5cca15c18add282
Tochukwu92 <Ilonubatochukwu@gmail.com>
2024-10-11 23:48:02 +0100

    python anotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''takes in a float and returns a string'''
+
+
+def to_str(n: float) -> str:
+    ''''
+    to_str - takes in a float and returns a string
+    '''
+    return str(n)
