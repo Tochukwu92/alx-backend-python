5b422515932f3b17b07ac3fba621587df8185111
Tochukwu92 <Ilonubatochukwu@gmail.com>
2024-10-11 23:11:51 +0100

    python anotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''add two numbers and return their sum'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    add - sum two integers and returns their sum
+    '''
+    return a + b
