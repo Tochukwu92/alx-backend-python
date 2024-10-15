f6eff4250663e0084ad031fe0b9588ee955241e6
Tochukwu92 <Ilonubatochukwu@gmail.com>
2024-10-15 15:35:08 +0100

    python async basic

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''return asycio.Task'''
+
+
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Return an asyncio.Task that wraps the wait_random coroutine.
+    """
+    return asyncio.create_task(wait_random(max_delay))
